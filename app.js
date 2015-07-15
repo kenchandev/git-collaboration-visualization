e718e1908a0fbb51267b7d72f4a2efbc15a95bce
Ken Chan <KChan923@gmail.com>
2015-07-15 16:36:14 -0400

    removed a line

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var User = require('./user.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var auth = require('./authentication.js');
-// var config = require('./config.js');
 // connect to the database
 mongoose.connect('mongodb://' + 'root' + ':' + 'github-network' + '@ds045511.mongolab.com:45511/github-network');
 
