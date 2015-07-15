ccf758775a20a3a8f7e6191ddb5dbdd69e39092d
Ken Chan <KChan923@gmail.com>
2015-07-15 17:33:51 -0400

    fixed callback url

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
 	github: {
 		clientID: 'cb0a3f3bc50ae1771b0d',
 		clientSecret: 'a11810e8ce3a00fa21b01e11ec70e7234a369abd',
-		callbackURL: 'http://github-network.herokuapp.com/'
+		callbackURL: 'http://github-network.herokuapp.com/auth/github/callback'
 	}
 };
 
