19e4d2cae64e3fb009749f5ad6bf6199949ab833
Ken Chan <KChan923@gmail.com>
2015-07-15 16:30:24 -0400

    removed config file for now

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var User = require('./user.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var auth = require('./authentication.js');
-var config = require('./config.js');
+// var config = require('./config.js');
 // connect to the database
 mongoose.connect('mongodb://' + 'root' + ':' + 'github-network' + '@ds045511.mongolab.com:45511/github-network');
 
